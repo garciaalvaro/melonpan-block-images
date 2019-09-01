9938443aea0a6d3c164d1866792ac854b520ab00
Alvaro <4710040+garciaalvaro@users.noreply.github.com>
2019-09-01 13:08:33 +0200

    Moved release to the root.

--- .webpack/copy.config.babel.js
+++ .webpack/copy.config.babel.js
@@ -5,7 +5,7 @@ module.exports = [
 	{
 		entry: __dirname + "/copy.entry.js",
 		output: {
-			path: __dirname + "/../_extras/release",
+			path: __dirname + "/../_release",
 			filename: "_temp.js"
 		},
 		plugins: [
@@ -16,6 +16,7 @@ module.exports = [
 						".*",
 						".*/**",
 						"_extras/**",
+						"_release/**",
 						"_temp.js",
 						"assets-repo/**",
 						`build/${name}-front.js`,
